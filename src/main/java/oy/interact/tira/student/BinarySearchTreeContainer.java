b29710bbea16772c4930d7c6001eda6d8a4bf73c
Teemu_Vaananen <teemvaan@gmail.com>
2023-11-04 15:06:26 +0200

    Task 07 continues...

--- src/main/java/oy/interact/tira/student/BinarySearchTreeContainer.java
+++ src/main/java/oy/interact/tira/student/BinarySearchTreeContainer.java
@@ -9,11 +9,11 @@ import oy.interact.tira.util.Visitor;
 
 public class BinarySearchTreeContainer<K extends Comparable<K>, V> implements TIRAKeyedOrderedContainer<K, V> {
 
-    private class TreeNode {
+    private class TreeNode<K extends Comparable<K>, V> {
         private K key;
         private V value;
-        private TreeNode left;
-        private TreeNode right;
+        TreeNode<K, V> left;
+        TreeNode<K, V> right;
 
         public TreeNode(K key, V value) {
             this.key = key;
@@ -24,7 +24,7 @@ public class BinarySearchTreeContainer<K extends Comparable<K>, V> implements TI
 
     }
 
-    TreeNode root;
+    TreeNode<K, V> root;
     private Comparator<K> comparator;
     int size;
 
@@ -34,36 +34,44 @@ public class BinarySearchTreeContainer<K extends Comparable<K>, V> implements TI
 
     @Override
     public void add(K key, V value) throws OutOfMemoryError, IllegalArgumentException {
-        if (key == null || value == null) {
+        if (key.equals(null) || value.equals(null)) {
             throw new IllegalArgumentException("The key or value was null");
         }
-        root = addHelper(root, key, value);
+        insertNode(root, key, value);
         size++;
     }
 
     @Override
     public V get(K key) throws IllegalArgumentException {
-        if (key == null) {
+        if (key.equals(null)) {
             throw new IllegalArgumentException("The key is null");
         }
-        return getHelper(root, key);
+
+        return search(root, key);
     }
 
     @Override
     public V remove(K key) throws IllegalArgumentException {
-        if (key == null) {
+        if (key.equals(null)) {
             throw new IllegalArgumentException("The key is null");
         }
 
-        root = removeHelper(root, key);
+        TreeNode<K, V> newRoot = deleteNode(root, key);
 
-        return root != null ? root.value : null;
+        if (!newRoot.equals(null)) {
+            size--;
+            root = newRoot;
+            return newRoot.value;
+        }
+        return null;
     }
 
     @Override
     public V find(Predicate<V> searcher) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'find'");
+      if(root.equals(null)){
+        return null;
+      }
+       return findNode(root, searcher);
     }
 
     @Override
@@ -121,50 +129,94 @@ public class BinarySearchTreeContainer<K extends Comparable<K>, V> implements TI
 
     // helper methods
 
-    private TreeNode addHelper(TreeNode node, K key, V value) {
-        if (node == null) {
-            return new TreeNode(key, value);
+    private TreeNode<K, V> insertNode(TreeNode<K, V> node, K key, V value) {
+        if (node.equals(null)) {
+            return new TreeNode<>(key, value);
         }
 
-        if (comparator.compare(key, node.key) < 0) {
-            node.left = addHelper(node.left, key, value);
-        } else if (comparator.compare(key, node.key) > 0) {
-            node.right = addHelper(node.right, key, value);
-        }
+        int comparatorResult = comparator.compare(node.key, key);
 
+        if (comparatorResult < 0) {
+            node.left = insertNode(node.left, key, value);
+        } else if (comparatorResult > 0) {
+            node.right = insertNode(node.right, key, value);
+        } else {
+            node.value = value;
+        }
         return node;
     }
 
-    private V getHelper(TreeNode node, K key) {
-        if (node == null) {
+    private V search(TreeNode<K, V> node, K key) {
+
+        if (node.equals(null)) {
             return null;
         }
+        int comparatorResult = comparator.compare(key, node.key);
 
-        if (comparator.compare(key, node.key) == 0) {
-            return node.value;
-        } else if (comparator.compare(key, node.key) < 0) {
-            return getHelper(node.left, key);
+        if (comparatorResult < 0) {
+            return search(node.left, key);
+        } else if (comparatorResult > 0) {
+            return search(node.right, key);
         } else {
-            return getHelper(node.right, key);
+            return node.value;
         }
     }
 
-        private TreeNode removeHelper(TreeNode node, K key){
-            if(node == null){
-                return node;
-            }
-            int comparatorResult = comparator.compare(key, node.key);
-
-            if(comparatorResult < 0){
-                node.left = removeHelper(node.left, key);
-            } else if(comparatorResult > 0){
-                node.right = removeHelper(node.right, key);
-            } else{
-                // todo: finish this, the key is found here to be deleted
-                //according to the amount of children the node has
-            }
+    private TreeNode<K, V> deleteNode(TreeNode<K, V> node, K key) {
+        if (node.equals(null)) {
             return node;
-            
         }
 
+        int comparatorResult = comparator.compare(node.key, key);
+
+        if (comparatorResult < 0) {
+            node.left = deleteNode(node.left, key);
+        } else if (comparatorResult > 0) {
+            node.right = deleteNode(node.right, key);
+        } else {
+            // the key is found here
+            if (root.left.equals(null)) {
+                return root.right;
+            } else if (root.right.equals(null)) {
+                return root.left;
+            }
+            // here we can presume that the node has two children so lets find the inorder
+            // predecessor(node with largest value in the left subtree)
+            TreeNode<K, V> maxValueTreeNode = findMaxNode(node.left);
+            node.key = maxValueTreeNode.key;
+            node.value = maxValueTreeNode.value;
+            node.left = deleteNode(node.left, node.key);
+
+        }
+        return node;
+    }
+
+    private TreeNode<K, V> findMaxNode(TreeNode<K, V> node) {
+        while (!node.right.equals(null)) {
+            node = node.right;
+        }
+        return node;
+    }
+
+
+    private V findNode(TreeNode<K, V> node, Predicate<V> explorer){
+        V leftSearch = findNode(node.left, explorer);
+        V rightSearch = findNode(node.right, explorer);
+
+        if(node.equals(null)){
+            return node.value;
+        }
+        
+        if(explorer.test(node.value)){
+            return node.value;
+        }
+        
+        if(leftSearch != null){
+            return leftSearch;
+        }
+        if(rightSearch != null){
+            return rightSearch;
+        }
+        return null;
+    }
 }
